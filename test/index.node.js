c9c8bdea13b0a0ea75a2620e37de01b747b190d8
uupaa <uupaa.js@gmail.com>
2014-01-27 14:08:34 +0900

    remove WordArray.js

--- test/index.node.js
+++ test/index.node.js
@@ -1,7 +1,6 @@
 require("uupaa.task.js");
 require("uupaa.test.js");
 require("uupaa.bytearray.js");
-require("uupaa.wordarray.js");
 require("uupaa.md5.js");
 require("uupaa.sha1.js");
 require("../lib/HMAC.js");
