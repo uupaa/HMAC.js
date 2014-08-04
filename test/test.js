479da3f0dfff36dfec3321d18c4ab997eb6961e5
uupaa <uupaa.js@gmail.com>
2014-08-05 02:41:46 +0900

    support TypedArray

--- test/test.js
+++ test/test.js
@@ -20,119 +20,121 @@ return new Test("HMAC", {
         testHMAC_SHA1,
     ]).run().clone();
 
-function testHMACMD5String(next) {
+function testHMACMD5String(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "74e6f7298a9c2d168935f58c001bad88";
     var HMAC_MD5 = HMAC("MD5", "", "");
 
     if (answer === HMAC_MD5) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testHMACMD5StringWithKey(next) {
+function testHMACMD5StringWithKey(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "80070713463e7749b90c2dc24911e275";
     var HMAC_MD5 = HMAC("MD5", "key", "The quick brown fox jumps over the lazy dog");
 
     if (answer === HMAC_MD5) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testHMACSHA1String(next) {
+function testHMACSHA1String(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "fbdb1d1b18aa6c08324b7d64b71fb76370690e1d";
     var HMAC_SHA1 = HMAC("SHA1", "", "");
 
     if (answer === HMAC_SHA1) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testHMACSHA1StringWithKey(next) {
+function testHMACSHA1StringWithKey(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9";
     var HMAC_SHA1 = HMAC("SHA1", "key", "The quick brown fox jumps over the lazy dog");
 
     if (answer === HMAC_SHA1) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testHMACMD5Binary(next) {
+function testHMACMD5Binary(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "74e6f7298a9c2d168935f58c001bad88";
-    var HMAC_MD5 = HMAC.encode("MD5", [], []);
+    var HMAC_MD5 = HMAC.encode("MD5", new Uint8Array(0), new Uint8Array(0));
 
-    var match = HMAC_MD5.every(function(value, index) {
+  //var match = HMAC_MD5.every(function(value, index) {
+    var match = Array.prototype.every.call(HMAC_MD5, function(value, index) {
             var hex = parseInt(answer.slice(index * 2, index * 2 + 2), 16);
 
             return value === hex;
         });
 
     if (match) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testHMACSHA1Binary(next) {
+function testHMACSHA1Binary(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "fbdb1d1b18aa6c08324b7d64b71fb76370690e1d";
-    var HMAC_SHA1 = HMAC.encode("SHA1", [], []);
+    var HMAC_SHA1 = HMAC.encode("SHA1", new Uint8Array(0), new Uint8Array(0));
 
-    var match = HMAC_SHA1.every(function(value, index) {
+  //var match = HMAC_SHA1.every(function(value, index) {
+    var match = Array.prototype.every.call(HMAC_SHA1, function(value, index) {
             var hex = parseInt(answer.slice(index * 2, index * 2 + 2), 16);
 
             return value === hex;
         });
 
     if (match) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testHMAC_MD5(next) {
+function testHMAC_MD5(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "74e6f7298a9c2d168935f58c001bad88";
     var hash = HMAC.MD5("", "");
 
     if (answer === hash) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testHMAC_SHA1(next) {
+function testHMAC_SHA1(test, pass, miss) {
 
     // this magic value from http://en.wikipedia.org/wiki/HMAC
     var answer = "fbdb1d1b18aa6c08324b7d64b71fb76370690e1d";
     var hash = HMAC.SHA1("", "");
 
     if (answer === hash) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
